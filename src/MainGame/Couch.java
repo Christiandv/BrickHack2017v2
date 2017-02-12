4769362bcb60ec47511a483da1f1f8b37d32dfa9
ceb5002 <ceb5002@cs.rit.edu>
2017-02-11 19:58:20 -0500

    couch edit

--- src/MainGame/Couch.java
+++ src/MainGame/Couch.java
@@ -1,7 +1,15 @@
 package MainGame;
 
-/**
- * Created by Caitlin on 2/11/2017.
+/*
+ * File: Couch.java
+ *
+ * couch
+ *
+ * @author Sebastian Serrano-Berthet : sas3897 (github)
+ * @author Christian Vaughan : christiandv (github)
+ * @author Josef Lenz : JosefLenz (github)
+ * @author Caitlin Barron : CaitlinBarron (github)
+ *
  */
 public class Couch {
     public Couch(int x, int y)
