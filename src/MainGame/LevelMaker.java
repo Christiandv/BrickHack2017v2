66c6ee575563fc876450b51e0c10ef4fad21d1ff
sas3897 <sas3897@g.rit.edu>
2017-02-11 23:35:07 -0500

    Yep

--- src/MainGame/LevelMaker.java
+++ src/MainGame/LevelMaker.java
@@ -14,14 +14,13 @@ public class LevelMaker {
     public ArrayList<Sprite> getLevel1(){
         ArrayList<Sprite> level = new ArrayList<Sprite>();
 
-        for(int i = 0; i < 10; i++){
-            level.add(new Lava( i* 640, 399));
-        }
+
         //Begin hardcoding
         level.addAll( new Couch(10, 313).getSprites());
         level.addAll(new Chair(308,312).getSprites());
-        level.add(new Shelf(360, 290));
-        level.addAll(new Chandelier(500, 240).getSprites());
+        level.add(new Shelf(425, 270));
+        level.add(new Bookcase(540, 271));
+
 
         Sprite fireball = new Sprite(700, 200);
         fireball.loadImage("media/images/fireballTest.gif");
@@ -30,6 +29,10 @@ public class LevelMaker {
         fireball.setHard();
 
         level.add(fireball);
+
+        for(int i = 0; i < 10; i++){
+            level.add(new Lava( i* 640, 399));
+        }
         return level;
     }
 
