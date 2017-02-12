fc51cb55a3680f3b8714c48c52545e3cb6a1b160
Christian <Christian.david.vaughan@gmail.com>
2017-02-11 23:58:44 -0500

    derpy shelf

--- src/MainGame/Shelf.java
+++ src/MainGame/Shelf.java
@@ -5,13 +5,26 @@ package MainGame;
  */
 public class Shelf extends Sprite
 {
+    int maxY;
+    boolean down = false;
     public Shelf (int x, int y)
     {
         super(x, y);
-
+        maxY = y;
         init();
     }
 
+    public void update(){
+        if(down) {
+            y--;
+            if (y < 0)
+                y = 0;
+        } else {
+            y++;
+            if(y > maxY)
+                y = maxY;
+        }
+    }
     public void init(){
         hard = true;
         loadImage("media/images/shelf.png");
